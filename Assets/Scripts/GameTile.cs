d9004c407c56a278927e467f8707bba5198421a6
Olivia <83168370+OliviaGoBrrr@users.noreply.github.com>
2025-08-20 19:27:02 +1000

    Code base started

--- Assets/Scripts/GameTile.cs
+++ Assets/Scripts/GameTile.cs
@@ -1,8 +1,53 @@
 using UnityEngine;
 
-public struct GameTile
+public class GameTile: MonoBehaviour
 {
-    public GameObject Tile;
-    public bool burnable;
-    public float[] burnProbTable;
+    [SerializeField] private GridManager gridManager;
+    [SerializeField] private int roundsToBurn;
+    [SerializeField] private bool canBeBurned;
+    [SerializeField] private bool isBurning;
+    private Vector3 tilePosition;
+
+
+    private GameTileData tileData;
+
+    // Placeholder
+    private float burnTimer = 10.0f;
+
+    void Awake()
+    {
+        tilePosition = transform.position;
+        tileData = new GameTileData(roundsToBurn, canBeBurned, isBurning, tilePosition);
+    }
+
+    void Update()
+    {
+        if (isBurning)
+        {
+            burnTimer -= Time.deltaTime;
+            
+            if (burnTimer < 0)
+            {
+                burnTimer = 10.0f;
+            }
+        }
+    }
 }
+
+public struct GameTileData
+{
+    public GameTileData(int burnRounds, bool canBeBurned, bool isBurning, Vector3 tilePosition)
+    {
+        this.IsBurning = isBurning;
+        this.CanBeBurned = canBeBurned;
+        this.RoundsToBurn = burnRounds;
+        this.TilePosition = tilePosition;
+    }
+
+    public int RoundsToBurn;
+    public bool CanBeBurned;
+    public bool IsBurning;
+
+    public Vector3 TilePosition;
+}
+
---
+++ Assets/Scripts/GridManager.cs
@@ -0,0 +1,71 @@
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class GridManager : MonoBehaviour
+{
+    [SerializeField] private Vector2Int gridSize;
+    [SerializeField] private Vector2Int gridGap;
+    [SerializeField] private Tilemap tilemap;
+
+    private bool requiresGeneration = true;
+    //private Camera playerCam;
+    private Grid grid;
+
+    // Grid Bounds
+    private int xMin;
+    private int xMax;
+    private int zMin;
+    private int zMax;
+
+    private List<GameTile> tiles = new List<GameTile>();
+    private GameTileData[,] masterTileGrid;
+
+    private Vector2 currentGap;
+    private Vector2 gapVel;
+
+    void Awake()
+    {
+        grid = GetComponent<Grid>();
+        tilemap = GetComponentInChildren<Tilemap>();
+        //playerCam = Camera.main;
+        currentGap = gridGap;
+
+        Debug.Log("Editor Started");
+
+        foreach(var tile in tilemap.GetComponentsInChildren<GameTile>())
+        {
+            Debug.Log("A tile has been added to the tile list");
+            tiles.Add(tile);
+
+            // Set horizontal bounds of the grid
+            if (tile.transform.position.x > xMax) { xMax = (int)tile.transform.position.x; }
+            else if(tile.transform.position.x < xMin) { xMin = (int)tile.transform.position.x; };
+
+            // Set vertical bounds of the grid
+            if(tile.transform.position.z > zMax) { zMax = (int)tile.transform.position.z;}
+            else if(tile.transform.position.z < zMin) { zMin = (int)tile.transform.position.z; };
+        }
+
+        if(tiles.Count <= 0)
+        {
+            Debug.LogError("No tiles found");
+        }
+        else
+        {
+            GenerateGrid();
+        }
+    }
+
+    // When the something in the editor changes, update the map (Editor Only)
+    private void OnValidate() => requiresGeneration = true;
+
+    void GenerateGrid()
+    {
+        Debug.Log($"Grid Bounds = minBounds: X{xMin},Z{zMin} + maxBounds: X{xMax},Z{zMax}");
+        Debug.Log("Generating Grid...");
+        
+        //var coordinates = new List<Vector3Int>();
+    }
+}
