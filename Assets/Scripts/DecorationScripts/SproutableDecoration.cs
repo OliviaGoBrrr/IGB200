a810736c9b80afa52a6d07af6c27f9bdf61d9e89
ThePinkOtter <SomePinkOtter@gmail.com>
2025-10-14 18:31:43 +1000

    Fixes & Flowers

--- Assets/Scripts/DecorationScripts/SproutableDecoration.cs
+++ Assets/Scripts/DecorationScripts/SproutableDecoration.cs
@@ -1,14 +1,18 @@
 using DG.Tweening;
+using NUnit.Framework;
 using UnityEngine;
+using System.Collections.Generic;
 
 public class SproutableDecoration : MonoBehaviour
 {
     private SpriteRenderer sprite2D;
     [SerializeField] ParticleSystem triggerPar;
     private bool triggered = false;
+    [SerializeField] List<Sprite> flowerOptions;
     void Awake()
     {
         sprite2D = GetComponent<SpriteRenderer>();
+        sprite2D.sprite = flowerOptions[Random.Range(0, flowerOptions.Count)];
         foreach (Transform childTransform in transform)
         {
             GameObject child = childTransform.gameObject;
